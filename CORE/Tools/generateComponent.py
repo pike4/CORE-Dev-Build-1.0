c0a82cff622b994f638820fe5d98f4a5653eb6f7
unknown <pike4@purdue.edu>
2018-01-29 23:03:37 -0500

    added python scripts for code generation

---
+++ CORE/Tools/generateComponent.py
@@ -0,0 +1,29 @@
+import os
+from addIncludeEntry import addInclude
+from addFactoryEntry import addFactory
+from generateComponentClassDefinition import genCompClassDef
+from generateComponentImplementation import genCompImpl
+
+os.chdir("..")
+path = os.getcwd()
+
+print(path)
+className = input("Enter the name of the new component: ")
+sourcePath = path + "\\Source"
+path = path + "\\Source\\Components\\User_Defined\\" + className
+
+path = path.replace("\\", "/")
+sourcePath = sourcePath.replace("\\", "/")
+
+
+#Write a skeleton class definition
+genCompClassDef(className, path)
+
+#Write skeleton member function implementations to the source file
+genCompImpl(className, path)
+
+#Add an entry to the includes manifest
+addInclude(className, sourcePath)
+
+#Add an entry to the factory constructor
+addFactory(className, sourcePath)
